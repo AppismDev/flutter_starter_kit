e361eef3224b5e22e4e1310019625a2146db4ff3
kerimbr <dev.kerimbora@gmail.com>
2022-10-02 17:15:48 +0300

    fsk_icons.dart (export file)

---
+++ lib/icons/fsk_icons.dart
@@ -0,0 +1,18 @@
+
+
+export 'ant_desing.dart';
+export 'entypo.dart';
+export 'evil_icons.dart';
+export 'feather.dart';
+export 'font_awesome.dart';
+export 'font_awesome_5_brands.dart';
+export 'font_awesome_5_regular.dart';
+export 'font_awesome_5_solid.dart';
+export 'fontisto.dart';
+export 'foundation.dart';
+export 'ionicons.dart';
+export 'material_community_icons.dart';
+export 'material_icons.dart';
+export 'octicons.dart';
+export 'simple_line_icons.dart';
+export 'zocial.dart';
