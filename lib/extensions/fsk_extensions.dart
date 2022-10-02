5e0cc2b379dc7d71e1835907a8ad68d28831d147
ahmetbugraozcan <bugra.ahmetozcan@gmail.com>
2022-10-02 18:38:15 +0300

    String extensions created.

--- lib/extensions/fsk_extensions.dart
+++ lib/extensions/fsk_extensions.dart
@@ -2,3 +2,4 @@ export 'arithmetic_extensions/arithmetic_extensions.dart';
 export 'context_extensions/context_extensions.dart';
 export 'date_time_extensions/datetime_extension.dart';
 export 'other_extensions/duration_extensions.dart';
+export 'string_extensions/string_extensions.dart';
---
+++ lib/extensions/string_extensions/string_extensions.dart
@@ -0,0 +1,51 @@
+extension StringExtensions on String {
+  /// Capitalizes first letter of this [String]
+  ///
+  /// ```dart
+  /// print('flutter'.capitalizeFirstLetter) // Flutter
+  /// print('Flutter'.capitalizeFirstLetter) // Flutter
+  /// ```
+
+  String get capitalizeFirstLetter =>
+      isNotEmpty ? this[0].toUpperCase() + substring(1) : this;
+
+  /// Capitalize first letters of this [String]
+  ///
+  /// ```dart
+  /// print('flutter starter kit'.capitalizeFirstLetter) // Flutter Starter Kit
+  /// print('Flutter Starter Kit'.capitalizeFirstLetter) // Flutter Starter Kit
+  /// ```
+  String get capitalizeFirstLetters {
+    return split(' ')
+        .map((word) => word.substring(0, 1).toUpperCase() + word.substring(1))
+        .join(' ');
+  }
+
+  /// Decapitalizes first letter of this [String]
+  ///
+  /// ```dart
+  /// print('Flutter'.capitalizeFirstLetter) // flutter
+  /// print('flutter'.capitalizeFirstLetter) // flutter
+  /// ```
+  String get decapitalizeFirstLetter =>
+      isNotEmpty ? this[0].toLowerCase() + substring(1) : this;
+
+  /// Capitalize first letters of this [String]
+  ///
+  /// ```dart
+  /// print('Flutter Starter Kit'.capitalizeFirstLetter) // flutter starter kit
+  /// print('flutter starter kit'.capitalizeFirstLetter) // flutter starter kit
+  /// ```
+  String get decapitalizeFirstLetters {
+    return split(' ')
+        .map((word) => word.substring(0, 1).toLowerCase() + word.substring(1))
+        .join(' ');
+  }
+
+  /// Returns a bool value indicating whether this [String] is empty or not
+  ///
+  ///```dart
+  /// print(''.isBlank) // true
+  ///```
+  bool get isBlank => trimLeft().isEmpty;
+}
